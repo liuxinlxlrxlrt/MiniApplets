73c3680bcd741121bc0bbe34374aa9fd45ce7d0d
liuxin <183771798@qq.com>
2020-08-31 21:59:19 +0800

    20200831-01

---
+++ src/test/java/com/imooc/MiniApplets/MiniAppletsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.MiniApplets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAppletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
